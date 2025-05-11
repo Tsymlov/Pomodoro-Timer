7b9d2f0fe0496f6804a2fdb9c783b922ce5061cb
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2025-05-11 21:15:46 +0300

    Initial Commit

---
+++ Pomodoro TimerTests/Pomodoro_TimerTests.swift
@@ -0,0 +1,16 @@
+//
+//  Pomodoro_TimerTests.swift
+//  Pomodoro TimerTests
+//
+//  Created by Alexey Tsymlov on 11.05.2025.
+//
+
+import Testing
+
+struct Pomodoro_TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
