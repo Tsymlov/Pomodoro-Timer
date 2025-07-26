092986271d49b78a5d408fc7ea827e5b04febf49
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2025-07-26 23:37:12 +0300

    Add a main view

---
+++ Pomodoro Timer/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Pomodoro Timer
+//
+//  Created by Alexey Tsymlov on 26.07.2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
