0e91bec98115b90960770e7a5b4cbd30993efedc
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2025-08-02 22:13:53 +0300

    Add constants

---
+++ Pomodoro Timer/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Pomodoro Timer
+//
+//  Created by Alexey Tsymlov on 02.08.2025.
+//
+
+import Foundation
+
+private enum Constants {
+    static let pomodoroDuration: TimeInterval = 25 * 60 // 25 minutes
+    static let shortBreakDuration: TimeInterval = 5 * 60 // 5 minutes
+    static let longBreakDuration: TimeInterval = 15 * 60 // 15 minutes
+    static let timeInterval: TimeInterval = 1 // 1 second
+    static let pomodorosUntilLongBreak = 4
+}
