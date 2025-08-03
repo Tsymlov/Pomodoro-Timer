aae505ce100b62ad70ac18c12cc2f6cd53daaf1d
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2025-08-03 14:42:21 +0300

    Replace the content view to main view

--- Pomodoro Timer/Pomodoro_TimerApp.swift
+++ Pomodoro Timer/Pomodoro_TimerApp.swift
@@ -25,7 +25,7 @@ struct Pomodoro_TimerApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
         .modelContainer(sharedModelContainer)
     }
